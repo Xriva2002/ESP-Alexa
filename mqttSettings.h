f1f5bac269f5ef2161a1ad4708c2854d59a509d6
Xriva2002 <Xriva2002@users.noreply.github.com>
2020-04-21 10:07:50 +0100

    Add files via upload

---
+++ mqttSettings.h
@@ -0,0 +1,8 @@
+// MQTT Defines
+#define MQTT_MAX_PACKET_SIZE 1024
+
+#define MQTTSERVER      "192.168.1.101"                     // Home Server
+#define MQTTSERVERPORT  1883
+#define MQTTCLIENTID    ""                                  // Leave blank to auto generate one
+#define MQTTUSER        "admin"
+#define MQTTPASSWORD    "admin"
