aa87540b7167ff8a7b522c6684dcb4ae86fc0e74
Xriva2002 <Xriva2002@users.noreply.github.com>
2020-04-21 10:08:30 +0100

    Update mqttSettings.h

--- mqttSettings.h
+++ mqttSettings.h
@@ -4,5 +4,5 @@
 #define MQTTSERVER      "192.168.1.101"                     // Home Server
 #define MQTTSERVERPORT  1883
 #define MQTTCLIENTID    ""                                  // Leave blank to auto generate one
-#define MQTTUSER        "admin"
-#define MQTTPASSWORD    "admin"
+#define MQTTUSER        "username"
+#define MQTTPASSWORD    "password"
