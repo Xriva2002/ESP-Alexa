9fba22f70308a4aa76c8e5254ebacaf7962d4885
Xriva2002 <Xriva2002@users.noreply.github.com>
2020-05-04 17:38:44 +0100

    Update mqttSettings.h

--- mqttSettings.h
+++ mqttSettings.h
@@ -1,6 +1,4 @@
 // MQTT Defines
-#define MQTT_MAX_PACKET_SIZE 1024
-
 #define MQTTSERVER      "192.168.1.101"                     // Home Server
 #define MQTTSERVERPORT  1883
 #define MQTTCLIENTID    ""                                  // Leave blank to auto generate one
